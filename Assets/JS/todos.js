2f37bfe627a57fd5319b2c9d889864ac14fdb7c8
Fuad-Hellscream <49075537+Fuad-Hellscream@users.noreply.github.com>
2020-08-28 13:15:32 +0600

    Added ability to add new items to the todo list which work the same
    Basically completed all JavaScript work planned for this list. Mostly. The list now functions completely as intended. But the styling needs more work next.

--- Assets/JS/todos.js
+++ Assets/JS/todos.js
@@ -1,12 +1,23 @@
 // Check off specific Todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
-})
+});
 
 // Click on x to delete todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+// Append a task into the Todo list
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		// Grab new todo text from input
+		let todoText = $(this).val();
+		$(this).val("");
+		// Create a new li and add to ul
+		$("ul").append("<li><span>x</span> " + todoText + "</li>");
+	}
 })
